3092106112ad8332b55c64c76be1fce8b1bd9f53
Rafael Amaral De Paula <rafael@lovelacetecnologia.com>
2017-10-13 11:58:32 -0300

    Java

--- leilao-veiculos-lovelacetecnologia/src/com/lovelacetecnologia/email/email.java
+++ leilao-veiculos-lovelacetecnologia/src/com/lovelacetecnologia/email/email.java
@@ -6,27 +6,20 @@ import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
 
 public class email {
-	
-	
 
-	public void enviar(String para , String assunto , String msg) throws EmailException{
-		
-		
+	public void enviar(String para, String assunto, String mensagem) throws EmailException {
+
 		Email email = new SimpleEmail();
-		
+
 		email.setHostName("smtp.gmail.com");
 		email.setSmtpPort(465);
-		email.setAuthenticator(new DefaultAuthenticator(" rafaelpachecoipda@gmail.com " ,"mapa12345"));
+		email.setAuthenticator(new DefaultAuthenticator(" rafaelpachecoipda@gmail.com ", "mapa12345"));
 		email.setSSLOnConnect(true);
 		email.setFrom("rafael@lovelacetecnologia.com");
 		email.setSubject(assunto);
-		email.setMsg(msg);	
+		email.setMsg(mensagem);
 		email.addTo(para);
 		email.send();
-		
+
 	}
-	
-	
-	
-	
 }
